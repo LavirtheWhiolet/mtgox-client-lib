d148440343449db2652175846efe48b347cb0d45
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2012-02-02 18:03:25 +0600

    Requirements are put into separate file.

---
+++ lib/requirements.rb
@@ -0,0 +1,8 @@
+# Requirements of this library are described in this file.
+
+require 'rubygems' rescue raise %Q{"Ruby Gems" is required to be installed}
+
+gem 'facets'
+gem 'faraday'
+gem 'web-socket-ruby'
+gem 'json'
