59bdb0d970075aee5727397f5df75607c5fa5ac3
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2012-02-06 03:12:55 +0600

    String.to_rational is enhanced slightly. And fixed.

--- lib/string/to_rational.rb
+++ lib/string/to_rational.rb
@@ -4,15 +4,19 @@ require 'mathn'
 class String
   
   # 
-  # converts this String to Rational or returns 0 if the String can not
+  # converts this String to Rational or returns 0 (or nil) if the String can not
   # be converted.
   # 
-  def to_rational
+  def to_rational(return_nil_on_error = false)
+    #
+    error_value =
+      if return_nil_on_error then nil
+      else 0; end
     #
     mantissa, exponent = self.split(/[Ee]/, 2)
     exponent ||= "0"
     # Parse mantissa.
-    return 0 unless /^[\-\+]?[0-9\.]+$/ === mantissa
+    return error_value unless /^[\-\+]?[0-9]*(\.[0-9]*)?$/ === mantissa
     integer_part, fractional_part = mantissa.split(".", 2)
     fractional_part ||= ""
     mantissa = Rational(
@@ -20,7 +24,7 @@ class String
       ("1" + "0" * fractional_part.length).to_i
     )
     # Parse exponent.
-    return 0 unless /^[\-\+]?[0-9]+$/ === exponent
+    return error_value unless /^[\-\+]?[0-9]+$/ === exponent
     exponent = exponent.to_i
     #
     return mantissa * Rational(10, 1) ** exponent
